7158226f7e15636103a9705432ba0d9d878cb8f7
Peter Robinson <peter@greenfiresoftware.com>
2020-06-04 20:14:58 -0400

    Update MyNativeActivity.java
    Allows T2D to work on Android 6.0.1 marshmallow

--- engine/compilers/android-studio/app/src/main/java/com/garagegames/torque2d/MyNativeActivity.java
+++ engine/compilers/android-studio/app/src/main/java/com/garagegames/torque2d/MyNativeActivity.java
@@ -7,7 +7,8 @@ import android.os.Bundle;
 
 public class MyNativeActivity extends NativeActivity {
   static {
-    System.loadLibrary("openal"); 
+	System.loadLibrary("torque2d");
+    System.loadLibrary("openal");
   }
 
   @Override
