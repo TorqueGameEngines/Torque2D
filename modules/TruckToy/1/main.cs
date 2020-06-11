d724ced909571eea955c487a0bac1ec6feb86426
marauder2k7 <ctrl-intelligence@live.co.uk>
2020-06-11 22:48:10 +0100

    Update main.cs

--- modules/TruckToy/1/main.cs
+++ modules/TruckToy/1/main.cs
@@ -58,11 +58,6 @@ function TruckToy::create( %this )
     addFlagOption("Front Wheel Drive", "setFrontWheelDrive", TruckToy.FrontWheelDrive, false, "Whether the motor on the front wheel is active or not." );
     addFlagOption("Rear Wheel Drive", "setRearWheelDrive", TruckToy.RearWheelDrive, false, "Whether the motor on the rear wheel is active or not." );
     addFlagOption("Rotate Camera", "setRotateCamera", TruckToy.RotateCamera, true, "Whether the rotate the camera that is mounted to the truck or not." );
-    
-	%pos1 = "1 2";
-	%pos2 = "2 1";
-	
-	echo(Vector2AreaNormalize(%pos1,pos2));
 	
     // Reset the toy.
     %this.reset();
