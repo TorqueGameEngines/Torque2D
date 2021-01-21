6cafcd040d260b907d1920d50112fa163fb90397
Peter Robinson <peter@greenfiresoftware.com>
2021-01-21 14:40:29 -0500

    VS2019 project crashing bug fixed.

---
+++ engine/compilers/VisualStudio 2019/resource.h
@@ -0,0 +1,16 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Torque 2D.rc
+//
+#define IDI_TORQUE2D                    107
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
